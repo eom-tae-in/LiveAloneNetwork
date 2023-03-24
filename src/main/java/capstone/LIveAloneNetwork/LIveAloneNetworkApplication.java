0580fbd42ea1e467e1dc72954c76431a08bef57e
eom-tae-in <eti0728@daum.net>
2023-03-25 01:23:55 +0900

    initial commit

---
+++ src/main/java/capstone/LIveAloneNetwork/LIveAloneNetworkApplication.java
@@ -0,0 +1,13 @@
+package capstone.LIveAloneNetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LIveAloneNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LIveAloneNetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/capstone/LIveAloneNetwork/LIveAloneNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.LIveAloneNetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LIveAloneNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
