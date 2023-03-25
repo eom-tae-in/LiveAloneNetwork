6bc3ae2dcd33f4493f985c810e25b73310cf84c0
eom-tae-in <eti0728@daum.net>
2023-03-25 14:08:29 +0900

    [feat] Add MemberInfo.class

---
+++ src/main/java/capstone/LIveAloneNetwork/domain/member/MemberInfo.java
@@ -0,0 +1,17 @@
+package capstone.LIveAloneNetwork.domain.member;
+
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class MemberInfo {
+
+    private String nickname;
+    private String email;
+}
