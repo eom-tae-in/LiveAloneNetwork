a87a98caf30799f5d4b5dbf459fb602168ba037d
eom-tae-in <eti0728@daum.net>
2023-03-25 14:31:46 +0900

    [feat] Add Title.class

---
+++ src/main/java/capstone/LIveAloneNetwork/domain/post/Title.java
@@ -0,0 +1,14 @@
+package capstone.LIveAloneNetwork.domain.post;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Title {
+
+    private String title;
+}
